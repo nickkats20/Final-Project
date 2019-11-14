2b2714ab84ddfc4626b98da4c3635cadfd5c496a
Kats <20KatsNi@fps.fmtdom.local>
2019-11-14 15:15:37 -0500

    11/14

---
+++ moneyOptimizer.java
@@ -0,0 +1,14 @@
+public class moneyOptimizer {
+    public static void main(String[] args) {
+        change(47.20);
+    }
+
+    public static void change(double moneyIn) {
+        if (moneyIn >= 20) {
+            int twenties = (int) (moneyIn / 20);
+            System.out.println(twenties);
+           
+        }
+//split into dollars and cents first then subdivide
+    }
+}
